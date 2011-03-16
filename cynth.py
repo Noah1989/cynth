66fe93dfbab58fdf3f844d6e4d784492070bc8c8
Stefan Noack <stefan.noack@software-concept.de>
2011-03-16 14:27:12 +0100

    proof of concept: "strange oscillator" algorithm

--- cynth.py
+++ cynth.py
@@ -1,4 +1,4 @@
-# just playing around with wave generation...
+# proof of concept: a very simple algorithm generates complex sound creations.
 
 import wave
 import math
@@ -6,11 +6,21 @@ import struct
 
 output = wave.open('test.wav', 'w')
 
-output.setnchannels(1)
+output.setnchannels(2)
 output.setsampwidth(2)
 output.setframerate(44100)
 
-for x in xrange(10000):
-    output.writeframes(struct.pack('h', int(math.sin(x/50.0)*32768)))
+x, y = 1.0, 0.0
+dx, dy = 0.0, 1.0
 
+for i in xrange(5000000):
+    dx -= x / (abs(x) + abs(y))
+    dy -= y / (abs(x) + abs(y))
+    x += dx
+    y += dy
+    if i % 5000 == 0:
+        print i, x, y, dx, dy
+    output.writeframes(struct.pack('h', int(x)))
+    output.writeframes(struct.pack('h', int(y)))
+    
 output.close()
